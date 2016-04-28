463cbd544b5b4288551e04821983a2f68b6e820d
evanlouden <evanlouden@gmail.com>
2016-04-28 13:32:46 -0400

    temp disable api

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,24 +1,24 @@
 class WelcomeController < ApplicationController
 
   def index
-    geolocation = HTTParty.get('http://ip-api.com/json')
-    unless geolocation.nil?
-      @state = geolocation["region"]
-      @city = geolocation["city"]
-      key = ENV["WUNDERGROUND_KEY"]
-      hourly_weather = HTTParty.get("http://api.wunderground.com/api/#{key}/hourly/q/#{@state}/#{@city}.json")
-      astronomy = HTTParty.get("http://api.wunderground.com/api/#{key}/astronomy/q/#{@state}/#{@city}.json")
-      hour = astronomy["sun_phase"]["sunset"]["hour"]
-      minute = astronomy["sun_phase"]["sunset"]["minute"]
-      @sunset = Time.new.change(hour: hour, min: minute).strftime("%l:%M %p")
-      @weather_data = []
-      4.times do |n|
-        @weather_data << {
-          temp: hourly_weather["hourly_forecast"][n]["temp"]["english"],
-          icon: hourly_weather["hourly_forecast"][n]["icon_url"],
-          pop: hourly_weather["hourly_forecast"][n]["pop"]
-        }
-      end
-    end
+    # geolocation = HTTParty.get('http://ip-api.com/json')
+    # unless geolocation.nil?
+    #   @state = geolocation["region"]
+    #   @city = geolocation["city"]
+    #   key = ENV["WUNDERGROUND_KEY"]
+    #   hourly_weather = HTTParty.get("http://api.wunderground.com/api/#{key}/hourly/q/#{@state}/#{@city}.json")
+    #   astronomy = HTTParty.get("http://api.wunderground.com/api/#{key}/astronomy/q/#{@state}/#{@city}.json")
+    #   hour = astronomy["sun_phase"]["sunset"]["hour"]
+    #   minute = astronomy["sun_phase"]["sunset"]["minute"]
+    #   @sunset = Time.new.change(hour: hour, min: minute).strftime("%l:%M %p")
+    #   @weather_data = []
+    #   4.times do |n|
+    #     @weather_data << {
+    #       temp: hourly_weather["hourly_forecast"][n]["temp"]["english"],
+    #       icon: hourly_weather["hourly_forecast"][n]["icon_url"],
+    #       pop: hourly_weather["hourly_forecast"][n]["pop"]
+    #     }
+    #   end
+    # end
   end
 end
