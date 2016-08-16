2a45dacb102e47a71f7f08c8ff268c01a2e64752
evanlouden <evanlouden@gmail.com>
2016-08-15 23:13:18 -0400

    fix hound issues

--- app/models/holescore.rb
+++ app/models/holescore.rb
@@ -4,10 +4,8 @@ class Holescore < ActiveRecord::Base
   belongs_to :match
 
   def assign_strokes(user, match)
-    if user == match.hero && (1..18) === match.hero_adj_handicap
-      if hole.difficulty <= match.hero_adj_handicap
-        self.strokes = 1
-      end
+    if user == match.hero && match.hero_adj_handicap > 0 && match.hero_adj_handicap <= 18
+      self.strokes = 1 if hole.difficulty <= match.hero_adj_handicap
     elsif user == match.hero && match.hero_adj_handicap > 18
       if (hole.difficulty + 18) <= match.hero_adj_handicap
         self.strokes = 2
@@ -15,10 +13,8 @@ class Holescore < ActiveRecord::Base
         self.strokes = 1
       end
     end
-    if user == match.villain && (1..18) === match.villain_adj_handicap
-      if hole.difficulty <= match.villain_adj_handicap
-        self.strokes = 1
-      end
+    if user == match.villain && match.villain_adj_handicap > 0 && match.villain_adj_handicap <= 18
+      self.strokes = 1 if hole.difficulty <= match.villain_adj_handicap
     elsif user == match.villain && match.villain_adj_handicap > 18
       if (hole.difficulty + 18) <= match.villain_adj_handicap
         self.strokes = 2
